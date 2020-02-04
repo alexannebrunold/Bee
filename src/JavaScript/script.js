f1eb9faff55903eaf91a640d93af42cf3cc7aecb
RomainDanizel <romain.danizel@hetic.net>
2020-02-04 10:23:51 +0100

    banner-mobile

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -1,15 +1,7 @@
-function loadPage(name, action) {
-    return fetch('../pages/' + name + '.html').then(function(response) {
-      if (response.ok) {
-        response.text().then(function(html) {
-          document.body.innerHTML = html;
-          document.body.setAttribute('class', name);
-        console.log("document.body", document.body)
-          if (action) {
-            action.call();
-          }
-        });
-      }
-    })}
 
-loadPage("landing")
+const burger = document.getElementById('burger__menu')
+const ul = document.querySelector('.lis')
+ 
+burger.addEventListener('click', function(){
+  burger.classList.toggle('is-open')
+})
