bac12ede7494f9df3813fe03b43c78bac874e8ef
Alexanne BRUNOLD <alexannebrunold@MacBook-Pro-de-Alexanne.local>
2020-02-06 19:22:37 +0100

    fix1

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -1,18 +1,8 @@
 
 const burger = document.getElementById('burger__menu')
 const ul = document.querySelector('.lis')
-<<<<<<< HEAD
-
-if (window.width <= 750) {
-burger.addEventListener('click', function () {
-  burger.classList.toggle('is-open')
-})
-}
-
-=======
 if (window.width <= 750) {
   burger.addEventListener('click', function () {
     burger.classList.toggle('is-open')
   })
 }
->>>>>>> 8343eb45b342adf72ff64b83637a18b7a82df9d7
